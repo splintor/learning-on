94608a72b22fbb7efa68ed868c424c9023d1bfe0
Shmulik Flint <shmulikf@wix.com>
2024-09-18 01:06:12 +0300

    enable https locally

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import { vercelPreset } from '@vercel/remix/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import mkcert from 'vite-plugin-mkcert';
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths(), mkcert()],
 });
